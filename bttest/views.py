c77f6c880a4162148f64703cf4061cfd4a2a008e
nikhil <nikhil@nikhil.(none)>
2014-03-03 15:17:25 +0530

    Added delete

--- bttest/views.py
+++ bttest/views.py
@@ -3,25 +3,15 @@ from django.shortcuts import render_to_response
 from django.shortcuts import RequestContext
 from forms import MemberForm
  
-def index(request, id):
-    #Passing member data from view, these data should be retrieved from database
-    members = [
+#Passing member data from view, these data should be retrieved from database
+members = [
         {'id': '1', 'name': 'McGiney, Mark', 'email': 'mark@marksautomark.com', 'status' : 'Administrator'},
         {'id': '2', 'name': 'Alan, George', 'email': 'george@marksautomark.com', 'status' : 'Employee'},
         {'id': '3', 'name': 'Joned, Bob', 'email': 'bob@marksautomark.com', 'status' : 'Employee'},
         {'id': '4', 'name': 'O\'Brieb, Barbara', 'email': 'barbara@marksautomark.com', 'status' : 'Employee'},
     ]
+def index(request, id):
     
-    if id:
-        index = 0
-        for member in members:
-            if member.id == id:
-                break
-            index = index + 1
-            
-        
-        del members[index]
-        return render_to_response('index.html', {'form': member_form, 'members': members}, context_instance=RequestContext(request))
     if request.method == 'POST':
         params = request.POST
         member_form = MemberForm(request.POST)
@@ -38,3 +28,15 @@ def index(request, id):
         return render_to_response('index.html', {'form': member_form, 'members': members}, context_instance=RequestContext(request))
     else:    
         return render_to_response('index.html', {'form': MemberForm(), 'members': members}, context_instance=RequestContext(request))
+    
+def delete(request, id):
+    if id:
+        index = 0
+        for member in members:
+            if member.id == id:
+                break
+            index = index + 1
+            
+        
+        del MEMBER[index]
+        return render_to_response('index.html', {'form': member_form, 'members': members}, context_instance=RequestContext(request))
